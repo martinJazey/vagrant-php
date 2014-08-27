739b793a458f37e13f712b4e12095c7124db1277
Bernhard Essl <bernhardessl@gmail.com>
2014-08-27 20:44:43 +0200

    Add bashrc and gitignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-echo 'alias ll="ls -la"' >> /home/vagrant/.bashrc
+# mysql root:root
 echo "mysql-server-5.5 mysql-server/root_password_again password root" | debconf-set-selections
 echo "mysql-server-5.5 mysql-server/root_password password root" | debconf-set-selections
 aptitude update
 aptitude install -y mysql-server mysql-client nginx php5-fpm vim php5-curl phpunit php5-mysql php-apc git
+
+# config nginx
+cp confs/nginx.conf /etc/nginx/sites-available/default
 /etc/init.d/nginx start
+
+cp confs/bashrc.conf /home/vagrant/.bashrc
+cp confs/gitignore_global.conf /home/vagrant/.gitignore_global
