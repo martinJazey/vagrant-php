c8723c37ff313cba3cc631ba253bc4dde68f7232
Bernhard Essl <bernhardessl@gmail.com>
2014-09-03 21:11:47 +0200

    Add full path to binaries

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 
 # mysql root:root
-echo "mysql-server-5.5 mysql-server/root_password_again password root" | debconf-set-selections
-echo "mysql-server-5.5 mysql-server/root_password password root" | debconf-set-selections
-aptitude update
-aptitude install -y mysql-server mysql-client nginx php5-fpm vim php5-curl phpunit php5-mysql php-apc git php5-gd drush libapache2-mod-php5
+/bin/echo "mysql-server-5.5 mysql-server/root_password_again password root" | /usr/bin/debconf-set-selections
+/bin/echo "mysql-server-5.5 mysql-server/root_password password root" | /usr/bin/debconf-set-selections
+/usr/bin/aptitude update
+/usr/bin/aptitude install -y mysql-server mysql-client nginx php5-fpm vim php5-curl phpunit php5-mysql php-apc git php5-gd drush libapache2-mod-php5
 
 # config nginx
-cp confs/nginx.conf /etc/nginx/sites-available/default
+/bin/cp confs/nginx.conf /etc/nginx/sites-available/default
 /etc/init.d/nginx restart
 
-cp confs/bashrc.conf /home/vagrant/.bashrc
+/bin/cp confs/bashrc.conf /home/vagrant/.bashrc
 
 # config git
-cp confs/gitignore_global.conf /home/vagrant/.gitignore_global
-cp confs/gitconfig.conf /home/vagrant/.gitconfig
+/bin/cp confs/gitignore_global.conf /home/vagrant/.gitignore_global
+/bin/cp confs/gitconfig.conf /home/vagrant/.gitconfig
 
 # config mysql
-cp confs/mysql.conf /etc/mysql/my.cnf
+/bin/cp confs/mysql.conf /etc/mysql/my.cnf
 /etc/init.d/mysql restart
 
 # config apache
-cp confs/apache.conf /etc/apache2/sites-enabled/000-default
+/bin/cp confs/apache.conf /etc/apache2/sites-enabled/000-default
 
 # config apache php.ini
-cp apache.php.conf /etc/php5/apache2/php.ini
+/bin/cp apache.php.conf /etc/php5/apache2/php.ini
 
 /etc/init.d/apache2 restart
 
 # timezone
-echo "Europe/Vienna" > /etc/timezone
-dpkg-reconfigure -f noninteractive tzdata
+/bin/echo "Europe/Vienna" > /etc/timezone
+/usr/sbin/dpkg-reconfigure -f noninteractive tzdata
